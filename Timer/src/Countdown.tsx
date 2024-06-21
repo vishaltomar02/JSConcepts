8137f1a8914cae8340c676cc26cbfd7df28e47ee
Vishal Tomar <vishal1.tomar@paytm.com>
2024-06-21 23:18:11 +0530

    reset functionality added, css added for buttons and issue handled for negative timer values and clearing interval when timer goes to 0

--- Timer/src/Countdown.tsx
+++ Timer/src/Countdown.tsx
@@ -16,12 +16,14 @@ const Countdown = () => {
   }
   
   useEffect(() => {
-    console.log(timerMinutes);
     let timerInterval = 0;
     if(timerMinutes && playing) {
       timerInterval = setInterval(() => {
-        setTimer(prev => prev - 1);
-      }, 1000);
+        setTimer(prev => {
+          !prev && clearInterval(timerInterval)
+          return prev === 0 ? prev : prev - 1
+        });
+      }, 100);
     }
     else {
       clearInterval(timerInterval)
@@ -37,7 +39,6 @@ const Countdown = () => {
   const timerText = useMemo(getCountDownTime, [timer]);
 
   function handleTimerInput(e:React.SyntheticEvent) {
-    console.log("changed")
     const target = e.target as typeof e.target & {
       value: string
     }
@@ -47,6 +48,12 @@ const Countdown = () => {
     setTimer(Number(target.value)*60)
   }
 
+  function handleReset() {
+    setPlaying(false);
+    setTimerMinutes(0);
+    setTimer(0);
+  }
+
   return (
     <main className="timer-container">
       <div className={styles.inputContainer}>
@@ -54,10 +61,11 @@ const Countdown = () => {
         <input type="text" placeholder="Enter duration" onChange={handleTimerInput} value={timerMinutes}/>
       </div>
       <div className={styles.timerHandlers}>
-        <button className={styles.iconBtn} onClick={() => setPlaying(prev => !prev)}>
+        <button className={`${styles.iconBtn} ${playing ? styles.active : ''}`} onClick={() => setPlaying(prev => !prev)}>
           <Icon/>
         </button>
         <span className={styles.timerText}>{timerText}</span>
+        <button className={`${styles.timerText} ${styles.resetBtn}`} type="button" name="reset" onClick={handleReset}>Reset</button>
       </div>
     </main>
   );
